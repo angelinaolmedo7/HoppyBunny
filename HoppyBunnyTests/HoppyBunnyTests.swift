a0d8ff55ff37fe3bb02ada4d416ed7c657cb0dbb
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-06-08 10:34:34 -0700

    Initial Commit

---
+++ HoppyBunnyTests/HoppyBunnyTests.swift
@@ -0,0 +1,34 @@
+//
+//  HoppyBunnyTests.swift
+//  HoppyBunnyTests
+//
+//  Created by Angelina Olmedo on 6/8/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import XCTest
+@testable import HoppyBunny
+
+class HoppyBunnyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
